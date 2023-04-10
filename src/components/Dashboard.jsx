ca820f3341d903932b9257a939f1fedbd50f0337
Anisha-Karmacharya <anisha.karmacharya.01@gmail.com>
2023-04-10 16:59:45 +0545

    fetch data from given api and displayed it in list view

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,17 +1,51 @@
+
+
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import React, { useEffect, useState } from 'react'
 
 const Dashboard = () => {
-    const [datas, setdata] = useState([]);
-    useEffect(() => {
+  const [data, setData] = useState('');
 
-    })
-    // const fetchData = () =>{
-    //     const response = axios.get('https://jp-dev.cityremit.global/web-api/transaction-manager/v1/admin/dashboard/search')
-    // }
-  return (
-    <>Dashoard</>
-  )
-}
+  useEffect(() => {
+    const fetchData = async () => {
+      const jwtToken = localStorage.getItem('jwtToken');
+      const res = await axios.post(
+        'https://jp-dev.cityremit.global/web-api/transaction-manager/v1/admin/dashboard/search',
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${jwtToken}`,
+          },
+        }
+      );
+      setData(res.data.data);
+      console.log(res.data.data)
+ // do something with response data
+    };
+    fetchData();
+  }, []);
+
+  return(
+    <div>
+        <h1>
+            Dashboard
+        </h1>
+        <ul>
+            {data.map((user) => (
+               <li key= {user.id}>
+                <p>{user?.['Current Status']}</p>
+                <p>{user?.['Receive Amount/受取金額']}</p>
+                <p>{user?.['Receive Country/受取国']}</p>
+                <p>{user?.['Receiver Full Name']}</p>
+                <p>{user?.['Send Amount/送金額']}</p>
+                <p>{user?.['Send Country/送金国']}</p>
+                <p>{user?.['Sender Full Name']}</p>
+                </li>
+              
+            ))}
+        </ul>
+    </div>
+  );
+};
 
 export default Dashboard;
